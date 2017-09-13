e0bfa627f5bd9b57eb6fbacc875513503f208f33
tumscorpion <pk.prateep@gmail.com>
2017-09-13 17:25:24 +0700

    merge

--- src/Hello.java
+++ src/Hello.java
@@ -8,14 +8,13 @@ public class Hello {
 		System.out.println("Hello Test2");
 		System.out.println("Hello test4");
 		System.out.println("Hello test5");
-<<<<<<< HEAD
+
 		System.out.println("test 7");
 		System.out.println("test 7");
 		System.out.println("test 7");
 		System.out.println("test 7");
 		System.out.println("test 7");
 		System.out.println("test 7");
-=======
 		
 		System.out.println("hello test6");
 		
@@ -24,7 +23,6 @@ public class Hello {
 		System.out.println("hello test6");
 		
 		System.out.println("hello test6");
->>>>>>> branch 'master' of https://github.com/TumPK/git-swe
 	}
 
 }
