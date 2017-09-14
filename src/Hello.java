18978051390eb53ef64d21b9b83952bf2b30e797
tumscorpion <pk.prateep@gmail.com>
2017-09-14 09:01:19 +0700

    change hello

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Hello TumPK");
 		
-		System.out.println("hello test");
+		System.out.println("hello test1");
 		
 	}
 
