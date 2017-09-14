7588c96ee997d34225f002efc221308e918d0b19
tumscorpion <pk.prateep@gmail.com>
2017-09-14 08:09:36 +0700

    reverse

--- src/Hello.java
+++ src/Hello.java
@@ -5,24 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello TumPK");
-		System.out.println("Hello Test2");
-		System.out.println("Hello test4");
-		System.out.println("Hello test5");
-
-		System.out.println("test 7");
-		System.out.println("test 7");
-		System.out.println("test 7");
-		System.out.println("test 7");
-		System.out.println("test 7");
-		System.out.println("test 7");
 		
-		System.out.println("hello test6");
-		
-		System.out.println("hello test6");
-		
-		System.out.println("hello test6");
-		
-		System.out.println("hello test6");
 	}
 
 }
